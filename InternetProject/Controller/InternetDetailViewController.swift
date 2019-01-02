6713ac56dc1808ef8bf2ab1cc9594bea083c756f
Skylar <skylarhimeno@gmail.com>
2019-01-02 09:36:29 -0700

    Updated InternetDetailViewController and then created new storyboard/ altered new storyboard

--- InternetProject/Controller/InternetDetailViewController.swift
+++ InternetProject/Controller/InternetDetailViewController.swift
@@ -9,6 +9,50 @@
 import UIKit
 import WebKit
 
+@UIApplicationMain
+public class AppDelegte: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate
+{
+    
+    var window: UIWindow?
+    
+    public func application(_ aaplication: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionKey: Any]?) -> Bool
+    {
+        let splitViewController = window!.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        splitViewController.delegate = self
+        return true
+    }
+    
+    public func applicationWillResignActive(_ application: UIApplication)
+    {<#code#>}
+    
+    public func applicationDidEnterBackground(_ application: UIApplication)
+    {<#code#>}
+    
+    public func applicationWillEnterForeground(_ application: UIApplication)
+    {<#code#>}
+    
+    public func applicationDidBecomeActive(_ application: UIApplication)
+    {<#code#>}
+    
+    public func applicationWillTerminate(_ application: UIApplication)
+    {<#code#>}
+    
+    // MARK: - Split View
+    public func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool
+    {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController
+            else
+        {return false}
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? InternetDetailViewController
+            else
+        {return false}
+        
+        return false
+    }
+}
+
 public class InternetDetailViewController: UIViewController
 {
     
